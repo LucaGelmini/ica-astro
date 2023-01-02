4a958bc7604ea2c37ab44255397d42724f7d2049
LucaGelmini <lucagelmini@hotmail.com>
2023-01-02 12:43:15 -0300

    colorScheme to tailwind extended theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				impo: colors.violet[400],
+				expo: colors.amber[400],
+				balance: colors.blue[400],
+
+			}
+		},
 	},
 	plugins: [],
 }
