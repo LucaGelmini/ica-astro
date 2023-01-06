d8098f49c76338a4e4c6d2fa8c26d47fc5976cf2
LucaGelmini <lucagelmini@hotmail.com>
2023-01-06 17:07:42 -0300

    adaptaciones a nuevos datos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,8 @@
 const colors = require('tailwindcss/colors');
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./src/data/dashboards/datos_barra_cajas.json'));
-const expoPercentage = data.bar.mensual.xPercentage;
-const impoPercentage = data.bar.mensual.mPercentage;
+const expoPercentage = data.bar.monthly.xPercentage * 100;
+const impoPercentage = data.bar.monthly.mPercentage * 100;
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
