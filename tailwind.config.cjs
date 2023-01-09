e96a3189e149a01a2aeb6d6744c575fd0227c4c5
LucaGelmini <lucagelmini@hotmail.com>
2023-01-09 13:05:38 -0300

    ADD: plotly lazy import & spinner placeholder

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
-const fs = require('fs');
-const data = JSON.parse(fs.readFileSync('./src/data/dashboards/datos_barra_cajas.json'));
-const expoPercentage = data.bar.monthly.xPercentage * 100;
-const impoPercentage = data.bar.monthly.mPercentage * 100;
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -14,10 +11,19 @@ module.exports = {
 				balance: colors.blue[400],
 			},
 			spacing: {
-				expo: expoPercentage + '%', impo: impoPercentage + '%',
+				...customSpacing()
 			}
 
 		},
 	},
 	plugins: [],
+}
+
+function customSpacing() {
+	const fs = require('fs');
+	const data = JSON.parse(fs.readFileSync('./src/data/dashboards/datos_barra_cajas.json'));
+	return ({
+		expo: String(data.bar.monthly.xPercentage * 100) + '%',
+		impo: String(impoPercentage = data.bar.monthly.mPercentage * 100) + '%'
+	})
 }
